edfa636a985adb799a7c90dd8570d02d39b8ab1c
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-04-14 15:10:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+const promise1 = new Promise((resolve) => setTimeout(resolve, 1000 * Math.floor((Math.random()*5)+1)));
+const promise2 = new Promise((resolve) => setTimeout(resolve, 1000 * Math.floor((Math.random()*5)+1))));
+const promise3 = new Promise((resolve) => setTimeout(resolve,1000 * Math.floor((Math.random()*5)+1))));
+const promise4 = new Promise((resolve) => setTimeout(resolve, 1000 * Math.floor((Math.random()*5)+1))));
+const promise5 = new Promise((resolve) => setTimeout(resolve,1000 * Math.floor((Math.random()*5)+1))));
+
+promises = [promise1, promise2, promise3,promise4,promise5];
